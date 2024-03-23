f0a49da6d333048a635bb6f8da6a9866432fb124
juhyoungRyu <thinkoutside2004@gmail.com>
2024-03-24 01:36:10 +0900

    router 구성 중

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { Constant } from "~/constant/Constant";
+
+let activeMenu: string;
+function setActiveMenu(menuIndex: number) {
+  activeMenu = Constant.SIDEBAR_ITEM[menuIndex].title;
+
+  console.log(menuIndex);
+}
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
+  <div id="App">
+    <Sidebar :setActiveMenu="setActiveMenu" :sidebarItem="[...Constant.SIDEBAR_ITEM]" />
+
+    <NuxtPage :currentTab="Constant.SIDEBAR_ITEM[0].title" />
   </div>
 </template>
+
+<style scoped>
+#App {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
